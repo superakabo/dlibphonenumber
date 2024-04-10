ae98a4ab760f31a9acf7f41bb7b2ea36fdd67520
superakabo <superakabo@gmail.com>
2024-04-10 08:03:50 +0000

    added some comments

--- lib/prefix_time_zones_map.dart
+++ lib/prefix_time_zones_map.dart
@@ -1,7 +1,7 @@
-///
-/// Copyright (C) 2012 The Libphonenumber Authors
+/// Based on the original Java code: .../phonenumbers/prefixmapper/PrefixTimeZonesMap.java
 ///
 /// [license]
+/// Copyright (C) 2012 The Libphonenumber Authors
 /// Licensed under the Apache License, Version 2.0 (the "License");
 /// you may not use this file except in compliance with the License.
 /// You may obtain a copy of the License at
