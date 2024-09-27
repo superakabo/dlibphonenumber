9feff4bced67961703c04afe2b623402e4d6bf2c
superakabo <superakabo@gmail.com>
2024-09-27 00:42:12 +0000

    updated release workflow

--- update_resources.sh
+++ update_resources.sh
@@ -6,9 +6,7 @@
 
 
 # Check for new liphonenumber release
-echo "checking for new release..."
 has_new_release=$(dart run tools/check_release.dart);
-echo "has new release -> $has_new_release";
 #
 # stop the process if there is no new release.
 if [ "$has_new_release" = false ]; then
