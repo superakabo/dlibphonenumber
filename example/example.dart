04f7491055de62c509c7b363cb4d69c9ee92aa5e
superakabo <superakabo@gmail.com>
2025-03-30 07:56:59 +0000

    Added some usage examples

--- example/example.dart
+++ example/example.dart
@@ -2,17 +2,24 @@ import 'package:dlibphonenumber/dlibphonenumber.dart';
 
 void main() {
   final PhoneNumberUtil phoneUtil = PhoneNumberUtil.instance;
-  final PhoneNumber phoneNumber = phoneUtil.parse('0241234567', 'GH');
-  final bool validPhoneNumber = phoneUtil.isValidNumber(phoneNumber);
-  final String e164 = phoneUtil.format(phoneNumber, PhoneNumberFormat.e164);
-  final String international = phoneUtil.format(phoneNumber, PhoneNumberFormat.international);
-  final String national = phoneUtil.format(phoneNumber, PhoneNumberFormat.national);
-  final String rfc3966 = phoneUtil.format(phoneNumber, PhoneNumberFormat.rfc3966);
+  final PhoneNumber phoneNumber1 = phoneUtil.parse('0241234567', 'GH');
+  final PhoneNumber phoneNumber2 = phoneUtil.parse('+233241234567', '');
+  final bool validPhoneNumber = phoneUtil.isValidNumber(phoneNumber1);
+  final String e164 = phoneUtil.format(phoneNumber1, PhoneNumberFormat.e164);
+  final String international = phoneUtil.format(phoneNumber1, PhoneNumberFormat.international);
+  final String national = phoneUtil.format(phoneNumber1, PhoneNumberFormat.national);
+  final String rfc3966 = phoneUtil.format(phoneNumber1, PhoneNumberFormat.rfc3966);
+  final PhoneNumberType numberType1 = phoneUtil.getNumberType(phoneNumber1);
+  final PhoneNumberType numberType2 = phoneUtil.getNumberType(phoneNumber2);
+  final MatchType matchType = phoneUtil.isNumberMatch(phoneNumber1, phoneNumber2);
+  final PhoneNumber? examplePhoneNumber = phoneUtil.getExampleNumber('GH');
+  final bool isPhoneNumber = phoneUtil.isPossibleNumber(phoneNumber2);
+  final bool isValidRegionNumber = phoneUtil.isValidNumberForRegion(phoneNumber1, 'US');
   final AsYouTypeFormatter asYouTypeFormatter = phoneUtil.getAsYouTypeFormatter('GH');
   final Iterable<PhoneNumberMatch> foundPhoneNumbers = phoneUtil.findNumbers('Call me on 0201234567', 'GH');
-  final String territory = PhoneNumberOfflineGeocoder.instance.getDescriptionForNumber(phoneNumber, Locale.english);
-  final List<String> timezones = PhoneNumberToTimeZonesMapper.instance.getTimeZonesForNumber(phoneNumber);
-  final String carrier = PhoneNumberToCarrierMapper.instance.getNameForNumber(phoneNumber, Locale.english);
+  final String territory = PhoneNumberOfflineGeocoder.instance.getDescriptionForNumber(phoneNumber1, Locale.english);
+  final List<String> timezones = PhoneNumberToTimeZonesMapper.instance.getTimeZonesForNumber(phoneNumber1);
+  final String carrier = PhoneNumberToCarrierMapper.instance.getNameForNumber(phoneNumber1, Locale.english);
 
   /// prints: +233241234567
   print('e164: $e164');
@@ -30,7 +37,28 @@ void main() {
   print('validPhoneNumber: $validPhoneNumber');
 
   /// prints: (countryCode: 233, nationalNumber: 241234567)
-  print('phoneNumber: $phoneNumber');
+  print('phoneNumber1: $phoneNumber1');
+
+  /// prints: (countryCode: 233, nationalNumber: 241234567)
+  print('phoneNumber2: $phoneNumber2');
+
+  /// prints: PhoneNumberType.mobile
+  print('phoneNumberType1: $numberType1');
+
+  /// prints: PhoneNumberType.mobile
+  print('phoneNumberType2: $numberType2');
+
+  /// prints: (countryCode: 233, nationalNumber: 302345678)
+  print('examplePhoneNumber: $examplePhoneNumber');
+
+  /// prints: MatchType.exactMatch
+  print('matchType: $matchType');
+
+  /// prints: true
+  print('isPhoneNumber: $isPhoneNumber');
+
+  /// prints: false
+  print('isValidRegionNumber: $isValidRegionNumber');
 
   /// prints: [countryCode: 233, nationalNumber: 201234567]
   print('foundPhoneNumbers: ${foundPhoneNumbers.map((e) => e.number)}');
